157148828db9ef596d731e0020a56d6c846e5e80
Nguyen Van Hoang <hoangnv2@fintech.smartosc.com>
2020-12-07 09:42:49 +0700

    data type in Python

--- demo/common.py
+++ demo/common.py
@@ -1,3 +1,5 @@
+import json
+
 class Parent:
     def override_method(self):
         print('parent "Override" method')
@@ -9,3 +11,82 @@ class Parent:
 class Child(Parent):
     def fn(self, a):
         print("this is a=", a)
+
+
+class Common:
+    def json(self):
+        x =  '{ "name":"John", "age":30, "city":"New York"}'
+        y = json.loads(x)
+        print(y["age"])
+
+    def json_dump(self):
+        x = {
+            "name": "John",
+            "age": 30,
+            "city": "New York"
+        }
+        y = json.dumps(x)
+        print(y)
+
+class List:
+    def make_alist(self):
+        alist = [1,2,3,"123",'one']
+        alist[0] = "11"
+        print("list=", alist)
+        alist.append("two")
+        print("after", alist)
+
+    def remove_duplicate(self):
+        l = [1,1,2,2,3]
+        s = set(l)
+        print("result after remove dupplicated value in list", list(s))
+
+# Orderd, unchangeble(Immutable) - not add, allow dupplicate
+class Tuple:
+    def make(self):
+        t = (5,1,1,2,'', None)
+        # is not tuple
+        t1= ("one value")
+        #t.append("2") -> raise error        
+        print("is tuple=", type(t1))
+        print("tuple", t)
+
+# Orderd, unchangeble(Immutable), NOT allow dupplicate
+class Set:
+    def make(self):
+        s = {5,1,2,'', None, None}
+        s1 = set((1,2, 3.4))
+        print("set={0}, {1}".format(s, s1))
+
+
+# unOrderd, changeble, NOT allow dupplicate
+class AnyObject:
+    def __init__(self, a, b):
+        self.aa = a
+        self.bb = b
+
+class Dict:
+    def make(self):
+        d = {"k1":5,"k2":"xxx", 1:2, "t":None, "t1":None}        
+        print("dictionary=", d[1]) 
+
+    def of(self, any_object):
+        print("aa={0}, b= {1}".format(any_object.aa, any_object.bb))
+
+class Ops:
+    def make(self) -> str:
+        ao1 = AnyObject(1,2)
+        ao2 = AnyObject(1,2)
+        ao3 = ao2
+        print("compare object={0}".format(ao1 is ao2))
+        print("compare value={0}, {1}".format(ao1 == ao2, ao2 == ao3))
+        try:
+            s = str(ao3)
+            del s
+        except TypeError as exp:
+            print('got exception')
+            raise TypeError('Errrrrrrrrrrr = ', exp)
+        
+        return ao3
+
+
--- test/ut.py
+++ test/ut.py
@@ -5,7 +5,7 @@ sys.path.append("./demo")
 import pkgb as b
 import pkga.obj1 as a1
 # import pkga.obj2 as a2 # --> make Error ModuleNotFoundError: No module named 'obj1' in obj2.py
-
+import common as c
 
 def main():
     b1 = b.Demo()
@@ -15,5 +15,36 @@ def main():
 
     # object2 = a2.Ob2()
     # print(object2.add(2,3))
+    c1 = c.Common()
+    # del c1
+    c1.json()
+    c.Common().json_dump()
+
+    #list
+    l = c.List()
+    l.make_alist()   
+    l.remove_duplicate()
+
+    #tuple
+    t = c.Tuple()
+    t.make()
+
+    #set
+    s = c.Set()
+    s.make()
+
+    #dict
+    d = c.Dict()
+    d.make()
+    anyobject = c.AnyObject("A", "B")
+    d.of(anyobject)
+
+    #Operation
+    o = c.Ops()
+    s1 = o.make()
+    print(s1)
+
+    r1 = "abc"[::-1]
+    print(r1)   
 
 main()    
